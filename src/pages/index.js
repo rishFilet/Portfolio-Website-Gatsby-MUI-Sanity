01fa670b0d6b117b799654be3f9a0421992084b4
Rishi Khan <rishi.rkhan@gmail.com>
2022-04-24 14:15:12 -0400

    Removed the ui placeholder code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Layout, Hero } from '@components';
-import { Placeholder } from 'semantic-ui-react';
 
 // styles
 
 function HomePage() {
-  useEffect(() => {
-    // eslint-disable-next-line no-undef
-    window.document.getElementById('ui placeholder').style.display = 'none';
-  }, []);
-
   return (
     <Layout>
-      <div id="ui placeholder">
-        <Placeholder>
-          <Placeholder.Header image>
-            <Placeholder.Line />
-            <Placeholder.Line />
-          </Placeholder.Header>
-          <Placeholder.Paragraph>
-            <Placeholder.Line />
-            <Placeholder.Line />
-            <Placeholder.Line />
-            <Placeholder.Line />
-          </Placeholder.Paragraph>
-        </Placeholder>
-      </div>
       <Hero />
     </Layout>
   );
